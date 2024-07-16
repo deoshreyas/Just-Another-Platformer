08ecce032a79b7234db2d27f17263a964430bd59
Shreyas Deo <shreyasdeo.k50@gmail.com>
2024-07-16 18:18:16 +0530

    Create the walking spike enemy and fix bugs in state changing from walk to attack

--- Scripts/spike_walking_enemy.gd
+++ Scripts/spike_walking_enemy.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@export var SPEED = 100
+
+@onready var sprite = $AnimatedSprite2D
 @onready var left = $Left 
 @onready var right = $Right 
 @onready var down1 = $Down1 
@@ -7,6 +10,29 @@ extends CharacterBody2D
 
 var direction = Vector2.LEFT
 
+var attack = false:
+	get:
+		return attack
+	set(value):
+		attack = value 
+		if attack:
+			sprite.play("attack")
+			position.y += 10
+			SPEED = 0
+		else:
+			position.y -= 10
+			sprite.play("walk") 
+			SPEED = 100
+
 func _physics_process(_delta): 
 	if left.is_colliding() or not down1.is_colliding():
 		direction = Vector2.RIGHT 
+		sprite.flip_h = true
+	elif right.is_colliding() or not down2.is_colliding():
+		direction = Vector2.LEFT 
+		sprite.flip_h = false 
+	velocity = SPEED * direction
+	move_and_slide()
+
+func _on_attack_timer_timeout():
+	attack = false if attack else true 
