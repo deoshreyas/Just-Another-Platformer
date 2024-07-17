7cbad022e0312b72a0f6e06df3210cd338574b37
Shreyas Deo <shreyasdeo.k50@gmail.com>
2024-07-18 00:25:12 +0530

    Created item spawner for walking enemy for coin and diamond with random probabilities

--- Scripts/big_walking_enemy.gd
+++ Scripts/big_walking_enemy.gd
@@ -10,6 +10,9 @@ extends CharacterBody2D
 var direction = Vector2.LEFT
 var alive = true
 
+var diamond = preload("res://Scenes/diamond.tscn")
+var coin = preload("res://Scenes/coin.tscn")
+
 func _physics_process(_delta):
 	if alive:
 		if left.is_colliding():
@@ -26,3 +29,20 @@ func _on_hurtbox_body_entered(body):
 		alive = false
 		sprite.play("dead")
 		hitbox.call_deferred("set_disabled", true)
+		spawn_item()
+
+func random_item():
+	var chance = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].pick_random()
+	if chance==1:
+		return diamond 
+	elif chance<4:
+		return coin 
+	else:
+		return false 
+
+func spawn_item():
+	var item = random_item()
+	if item:
+		var item_instance = item.instantiate()
+		item_instance.position = Vector2(-25, 0)
+		call_deferred("add_child", item_instance)
